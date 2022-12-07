0990588b9baaad35e4b0b300479af3f8ebb76e14
Riddhesh Ganatra <riddheshganatra@gmail.com>
2022-12-07 18:10:56 +0530

    auto theame fix

--- pages/_app.js
+++ pages/_app.js
@@ -3,13 +3,27 @@ import { AuthProvider } from '../AuthContext';
 import Navbar from '../components/Navbar';
 import '../styles/globals.css';
 
-const theme = createTheme({
-  palette: {
-    mode: 'dark'
-  }
-});
+// const theme = createTheme({
+//   palette: {
+//     mode: 'dark'
+//   }
+// });
 
 function MyApp({ Component, pageProps }) {
+
+  const prefersDarkMode = useMediaQuery('(prefers-color-scheme: dark)');
+
+  const theme = React.useMemo(
+    () =>
+      createTheme({
+        palette: {
+          mode: prefersDarkMode ? 'dark' : 'light',
+        },
+      }),
+    [prefersDarkMode],
+  );
+
+
   return (
     <AuthProvider>
       <ThemeProvider theme={theme}>
