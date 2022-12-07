6966a77225ee84fe27d9fcfc98ae9047bf49a3d7
Riddhesh Ganatra <riddheshganatra@gmail.com>
2022-12-07 19:17:13 +0530

    added comments

--- pages/api/airdrop.js
+++ pages/api/airdrop.js
@@ -2,6 +2,8 @@ import * as gariSdk from "gari";
 
 const gariClientId = "0319a8fc-b289-4a28-92f9-22ae141bd477";
 const gariSecretKey = "d41c1ac7-7671-41fa-94a8-ea79b67d01ea";
+
+// this wallet is clients wallet, below demo wallet has some garis of devnet for testing purpose
 const fromWalletPrivateKey =
   "03f6cac889d362c37589868ed67f068783fb433c7d0a0b907db24afd3a843e27d8c6f04cf5c6f33f2b83ba3e0e83e827bb305157105442c72a544c4e70c568b1";
 
