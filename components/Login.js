2582182d015f487d60d555b66cd5521e65f31a19
Nikhil Singh <nikhil.singh@codeb.online>
2022-12-09 14:25:09 +0530

    prettier

--- components/Login.js
+++ components/Login.js
@@ -7,27 +7,27 @@ import { useAuthContext } from "../AuthContext";
 import ReactLoading from "react-loading";
 // const gariSdk = require('gari-sdk')
 // import * as gariSdk from "gari-sdk";
-import * as gariSdk from 'gari'
+import * as gariSdk from "gari";
 
 export default function SignIn() {
-    const [{ token }, dispatch] = useAuthContext();
-    const [wallet, setWallet] = useState(null);
-    const [userId, setUserid] = useState(null);
-    const [publicKey, setPublicKey] = useState(null);
-    const [amount, setAmount] = useState(null);
-    const [sig, setSig] = useState(null);
-    const [isLoading, setLoading] = useState(false);
+  const [{ token }, dispatch] = useAuthContext();
+  const [wallet, setWallet] = useState(null);
+  const [userId, setUserid] = useState(null);
+  const [publicKey, setPublicKey] = useState(null);
+  const [amount, setAmount] = useState(null);
+  const [sig, setSig] = useState(null);
+  const [isLoading, setLoading] = useState(false);
 
-    const gariClientId = "0319a8fc-b289-4a28-92f9-22ae141bd477";
+  const gariClientId = "0319a8fc-b289-4a28-92f9-22ae141bd477";
 
-    async function getToken(userId) {
-        const response = await axios.get(
-            // `https://gari-sdk-front-end.vercel.app/api/login?userId=${userId}`
-            `/api/login?userId=${userId}`
-        );
-        console.log("response", response.data);
-        return response.data;
-    }
+  async function getToken(userId) {
+    const response = await axios.get(
+      `https://demo-gari-sdk.vercel.app/api/login?userId=${userId}`
+      // `/api/login?userId=${userId}`
+    );
+    console.log("response", response.data);
+    return response.data;
+  }
 
   async function handleAirdrop() {
     try {
@@ -36,16 +36,13 @@ export default function SignIn() {
       const publicKey = wallet.publicKey;
       const airdropAmount = 1;
 
-      const airdropResponse = await axios.post(
-        `/api/airdrop`,
-          {
-          headers: {
-            token,
-            publicKey,
-            airdropAmount
-          }}
-      );
-      console.log("airdropResponse ", airdropResponse);
+      // gariSdk.sdkInitialize(gariClientId, gariSecretKey)
+      const airdropSignature = await axios.post(`https://demo-gari-sdk.vercel.app/api/airdrop`, {publicKey, airdropAmount}, {
+        headers: {
+          token
+        },
+      });
+      console.log("airdropSignature ", airdropSignature);
       setLoading(false);
     } catch (error) {
       setLoading(false);
@@ -53,202 +50,173 @@ export default function SignIn() {
       return error;
     }
   }
-    async function handleLogin(event) {
-        event.preventDefault();
-        const token = await getToken(userId);
-        dispatch({ type: "login", token });
+  async function handleLogin(event) {
+    event.preventDefault();
+    const token = await getToken(userId);
+    dispatch({ type: "login", token });
+  }
+
+  async function handleTransaction(event) {
+    try {
+      event.preventDefault();
+      setLoading(true);
+      const encodedTransaction = await gariSdk.transferGariToken(
+        token,
+        publicKey,
+        amount,
+        gariClientId
+      );
+      console.log(
+        "encodedTransaction before main decode function ------> ",
+        encodedTransaction.encodedTransaction
+      );
+      
+      const transactionResponse = await gariSdk.initiateTransaction(
+        encodedTransaction.encodedTransaction,
+        token,
+        gariClientId
+      );
+      console.log("transactionResponse", transactionResponse);
+
+      setSig(transactionResponse?.data?.signature);
+      setLoading(false);
+    } catch (error) {
+      setLoading(false);
+      console.log("error", error);
     }
+  }
 
-    async function handleTransaction(event) {
-        try {
-            event.preventDefault();
-            setLoading(true);
-            const encodedTransaction = await gariSdk.transferGariToken(
-                token,
-                publicKey,
-                amount,
-                gariClientId
-            );
-            console.log(
-                "encodedTransaction before main decode function ------> ",
-                encodedTransaction.encodedTransaction
-            );
-            const tran = encodedTransaction.encodedTransaction;
-            const getDecode = await gariSdk.initiateTransaction(
-                tran,
-                token,
-                gariClientId
-            );
-            console.log("getDecode", getDecode);
+  async function getWalletDetails() {
+    try {
+      setLoading(true);
+      const walletRes = await gariSdk.createWalletOrGetWallet(token);
+      // const walletRes = await getWalletDetailsApi(gariClientId, token);
+      setWallet(walletRes);
+      console.log("wallet", wallet);
+      setLoading(false);
+    } catch (error) {
+      setLoading(false);
+      console.log("error", error);
+    }
+  }
 
-            setSig(getDecode?.data?.signature);
-            setLoading(false);
-        } catch (error) {
-            setLoading(false);
-            console.log("error", error);
+  useEffect(() => {
+    console.log(`gariSdk version ${gariSdk.packageVersion()}`);
+    gariSdk.sdkInitialize(gariClientId);
+  }, []);
+
+  return token ? (
+    <Container
+      component="form"
+      onSubmit={handleTransaction}
+      sx={{
+        display: "flex",
+        justifyContent: "center",
+        flexDirection: "column",
+        alignItems: "center",
+        p: 1,
+      }}
+    >
+      <div
+        style={{
+          position: "absolute",
+          left: "50%",
+          top: "50%",
+          zIndex: 1,
+        }}
+      >
+        {isLoading && (
+          <ReactLoading type={"spin"} color={"black"} height={667} />
+        )}
+      </div>
+      <h4 style={{ color: "black" }}>
+        PublicKey:{" "}
+        <span style={{ color: "green" }}>
+          {wallet == null ? "" : wallet.publicKey}
+        </span>
+      </h4>
+      <h4 style={{ color: "black" }}>
+        balance:{" "}
+        <span style={{ color: "green" }}>
+          {wallet == null ? "" : wallet.balance}
+        </span>
+      </h4>
+      <Button
+        type="button"
+        variant="contained"
+        sx={{ mt: 2, mb: 2 }}
+        onClick={getWalletDetails}
+      >
+        Get Wallet Details
+      </Button>
+      <Button
+        type="button"
+        variant="contained"
+        sx={{ mt: 2, mb: 2 }}
+        onClick={handleAirdrop}
+      >
+        Get airdrop
+      </Button>
+      <TextField
+        label="To Public Key"
+        variant="outlined"
+        name="publicKey"
+        margin="dense"
+        required
+        fullWidth
+        onChange={(e) => setPublicKey(e.target.value)}
+      />
+      <TextField
+        label="Amount"
+        variant="outlined"
+        name="amount"
+        margin="dense"
+        required
+        fullWidth
+        onChange={(e) => setAmount(e.target.value)}
+      />
+      <Button type="submit" variant="contained" sx={{ mt: 2 }}>
+        Make Transaction
+      </Button>
+      <h6 style={{ color: "black" }}>
+        Signature:{" "}
+        <span style={{ color: "red" }}>{sig == null ? "" : sig}</span>
+      </h6>
+      <Button
+        variant="contained"
+        sx={{ mt: 2 }}
+        onClick={() =>
+          dispatch({ type: "logout", message: "Logged Out Successfully" })
         }
-    }
-
-    async function handleAirdrop() {
-        try {
-            setLoading(true);
-            console.log(`getAirdrop called`);
-            // gariSdk.sdkInitialize(gariClientId, gariSecretKey)
-            // const receivedAirdrop = await gariSdk.airDrop(wallet.publicKey, airdropAmount, token, fromWalletPrivateKey)
-            const publicKey = wallet.publicKey;
-            const airdropAmount = 1;
-
-            const airdropResponse = await axios.post(
-                `/api/airdrop`,
-                {
-                    headers: {
-                        token,
-                        publicKey,
-                        airdropAmount
-                    }
-                }
-            );
-            console.log("airdropResponse ", airdropResponse);
-            setLoading(false);
-        } catch (error) {
-            setLoading(false);
-            console.log("error in airdrop function sdk ui", error);
-            return error;
-        }
-    }
-
-    async function getWalletDetails() {
-        try {
-            setLoading(true);
-            const walletRes = await gariSdk.createWalletOrGetWallet(token);
-            // const walletRes = await getWalletDetailsApi(gariClientId, token);
-            setWallet(walletRes);
-            console.log("wallet", wallet);
-            setLoading(false);
-        } catch (error) {
-            setLoading(false);
-            console.log("error", error);
-        }
-    }
-
-    useEffect(() => {
-
-        console.log(`gariSdk version ${gariSdk.packageVersion()}`);
-        gariSdk.sdkInitialize(gariClientId);
-    }, []);
-
-    return token ? (
-        <Container
-            component="form"
-            onSubmit={handleTransaction}
-            sx={{
-                display: "flex",
-                justifyContent: "center",
-                flexDirection: "column",
-                alignItems: "center",
-                p: 1,
-            }}
-        >
-            <div
-                style={{
-                    position: "absolute",
-                    left: "50%",
-                    top: "50%",
-                    zIndex: 1,
-                }}
-            >
-                {isLoading && (
-                    <ReactLoading type={"spin"} color={"black"} height={667} />
-                )}
-            </div>
-            <h4 style={{ color: "black" }}>
-                PublicKey:{" "}
-                <span style={{ color: "green" }}>
-                    {wallet == null ? "" : wallet.publicKey}
-                </span>
-            </h4>
-            <h4 style={{ color: "black" }}>
-                balance:{" "}
-                <span style={{ color: "green" }}>
-                    {wallet == null ? "" : wallet.balance}
-                </span>
-            </h4>
-            <Button
-                type="button"
-                variant="contained"
-                sx={{ mt: 2, mb: 2 }}
-                onClick={getWalletDetails}
-            >
-                Get Wallet Details
-            </Button>
-            <Button
-                type="button"
-                variant="contained"
-                sx={{ mt: 2, mb: 2 }}
-                onClick={handleAirdrop}
-            >
-                Get airdrop
-            </Button>
-            <TextField
-                label="To Public Key"
-                variant="outlined"
-                name="publicKey"
-                margin="dense"
-                required
-                fullWidth
-                onChange={(e) => setPublicKey(e.target.value)}
-            />
-            <TextField
-                label="Amount"
-                variant="outlined"
-                name="amount"
-                margin="dense"
-                required
-                fullWidth
-                onChange={(e) => setAmount(e.target.value)}
-            />
-            <Button type="submit" variant="contained" sx={{ mt: 2 }}>
-                Make Transaction
-            </Button>
-            <h6 style={{ color: "black" }}>
-                Signature:{" "}
-                <span style={{ color: "red" }}>{sig == null ? "" : sig}</span>
-            </h6>
-            <Button
-                variant="contained"
-                sx={{ mt: 2 }}
-                onClick={() =>
-                    dispatch({ type: "logout", message: "Logged Out Successfully" })
-                }
-            >
-                Log Out
-            </Button>
-        </Container>
-    ) : (
-        <Container
-            component="form"
-            onSubmit={handleLogin}
-            sx={{
-                display: "flex",
-                justifyContent: "center",
-                flexDirection: "column",
-                alignItems: "center",
-                p: 1,
-            }}
-        >
-            <TextField
-                label="UserId"
-                variant="outlined"
-                name="userId"
-                margin="dense"
-                required
-                fullWidth
-                type="number"
-                onChange={(e) => setUserid(e.target.value)}
-            />
-            <Button type="submit" variant="contained" sx={{ mt: 2 }}>
-                Login
-            </Button>
-        </Container>
-    );
+      >
+        Log Out
+      </Button>
+    </Container>
+  ) : (
+    <Container
+      component="form"
+      onSubmit={handleLogin}
+      sx={{
+        display: "flex",
+        justifyContent: "center",
+        flexDirection: "column",
+        alignItems: "center",
+        p: 1,
+      }}
+    >
+      <TextField
+        label="UserId"
+        variant="outlined"
+        name="userId"
+        margin="dense"
+        required
+        fullWidth
+        type="number"
+        onChange={(e) => setUserid(e.target.value)}
+      />
+      <Button type="submit" variant="contained" sx={{ mt: 2 }}>
+        Login
+      </Button>
+    </Container>
+  );
 }
--- pages/api/airdrop.js
+++ pages/api/airdrop.js
@@ -6,13 +6,12 @@ const gariSecretKey = "d41c1ac7-7671-41fa-94a8-ea79b67d01ea";
 // this wallet is clients wallet, below demo wallet has some garis of devnet for testing purpose
 const fromWalletPrivateKey =
   "03f6cac889d362c37589868ed67f068783fb433c7d0a0b907db24afd3a843e27d8c6f04cf5c6f33f2b83ba3e0e83e827bb305157105442c72a544c4e70c568b1";
-
 const fromWalletPublicKey=`FbD1J7ptwgSD8eCsyEm2TDVLnFwVhYRWkc2ingf6mn1n`
 
 export default async function handler(req, res) {
-  const publicKey = req.body.headers.publicKey;
-  const token = req.body.headers.token;
-  const airdropAmount = req.body.headers.airdropAmount;
+  const publicKey = req.body.publicKey;
+  const airdropAmount = req.body.airdropAmount;
+  const token = req.headers.token;
 
   gariSdk.sdkInitialize(gariClientId, gariSecretKey)
   
